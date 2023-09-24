8ddd3e810c0b821cf7f4af45cba9534b6aff4fd2
Debaditya <debadityasingh9@gmail.com>
2023-09-24 18:48:52 +0530

    Dark mode added

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let menuToggle = document.querySelector('.menuToggle');
+let lightDark = document.querySelector('.lightDark');
+let body = document.querySelector('body');
+let video = document.querySelector('video');
+let dark = false;
+let navigation = document.querySelector('.navigation');
+
+
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+};
+
+lightDark.onclick = function(){
+    dark = !dark;
+    lightDark.classList.toggle('active');
+    body.classList.toggle('dark');
+
+    if(dark){
+        video.setAttribute('src', "../assets/ice-forest-1.mp4");
+    } else {
+        video.setAttribute('src', "../assets/rain-forest-1.mp4");
+    }
+}
